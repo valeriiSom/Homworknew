f3a2dbb1d465826c57e0d4eea5a74a939f28f7a2
ValeraSom <lerek02@mail.ru>
2023-09-25 00:43:19 +0300

    homwork_03

---
+++ homwork_003/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Сколько вам нужно элементов?");
+Console.Write("возможно");
+int size = int.Parse(Console.ReadLine()!);
+int[] array = GetArray(size);
+PrintArray(array);
+
+int[] GetArray (int size)
+{
+  int[] array = new int[size];
+  for (int i = 0; i < size; i++)
+  {
+    array[i] = new Random().Next(1, 100);
+  }
+  return array;
+}
+
+void PrintArray(int[] array)
+{
+  Console.Write("[");
+  for (int i = 0; i < array.Length-1; i++)
+  {
+    Console.Write($"{array[i]}, ");
+  }
+  Console.Write($"{array[array.Length-1]}");
+  Console.WriteLine("]");
+}
